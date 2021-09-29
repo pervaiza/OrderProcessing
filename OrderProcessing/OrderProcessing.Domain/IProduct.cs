68f76783739fdbd83cf788106bdb5894eec2aa80
pervaiz akhtar <m.akhtar@hotmail.co.uk>
2021-09-29 12:38:47 +0100

    initial

---
+++ OrderProcessing/OrderProcessing.Domain/IProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OrderProcessing.Domain
+{
+    public interface IProduct
+    { }
+
+    public class PhysicalProduct : IProduct
+    { }
+
+    public class Book : IProduct
+    {}
+
+    public class Membership : IProduct
+    {}
+
+    public class UpgradeMemberShip : IProduct
+    { }
+
+    public class VideoLearning : IProduct
+    { }
+}
